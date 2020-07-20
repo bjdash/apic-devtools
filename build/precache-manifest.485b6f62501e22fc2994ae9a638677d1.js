7b8e97fedfb94d72f8d0b925dd7fcc2e0934eccb
Bijaya Dash <bijaya.k.dash@gmail.com>
2020-07-20 16:15:26 +0100

    Precommit build

---
+++ build/precache-manifest.485b6f62501e22fc2994ae9a638677d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28bfaf3e25a7586315fd3a92cea43f4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5fd734368267f2568009",
+    "url": "./static/css/main.4f9772be.chunk.css"
+  },
+  {
+    "revision": "84725344c3d2b78aff65",
+    "url": "./static/js/2.b8eef7bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b8eef7bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fd734368267f2568009",
+    "url": "./static/js/main.7e57d586.chunk.js"
+  },
+  {
+    "revision": "3677f43a90256b0e0b35",
+    "url": "./static/js/runtime-main.034061cd.js"
+  }
+]);
